9de0072e09cc6beeab383457311ef211208cb54a
MominIshtiaq <momin989ishtiaq@gmail.com>
2025-01-16 11:27:06 +0500

    move the paragraphs points to constants folder

--- src/container/AboutUs/OurStory.tsx
+++ src/container/AboutUs/OurStory.tsx
@@ -3,37 +3,9 @@ import Badge from "@/src/components/Badge";
 import { Box, Typography } from "@mui/material";
 import Image from "next/image";
 import Bull from "@/src/assets/images/Bull.png";
+import { paragraphs } from "@/src/const/aboutUsStory";
 
 const OurStory = () => {
-  const paragraphs = [
-    {
-      label: "Comprehensive Education",
-      points: [
-        "From online courses to in-person seminars, our curriculum caters to traders of all levels.",
-        "Beginners build a strong foundation with essential concepts, while advanced traders gain access to professional strategies like our copyrighted Breakout System, meticulously backtested for success.",
-        "With over 1,000 happy students, our academy remains one of the top-rated in the world.",
-      ],
-    },
-    {
-      label: "State-of-the-Art Technology",
-      points: [
-        "We lead the industry with innovative tools powered by AI technology, including automated bots and AI-driven signals.",
-        "Our mobile app and user-friendly website provide seamless access to these tools, ensuring traders can make informed and profitable decisions.",
-      ],
-    },
-    {
-      label: "Our Team: Expertise Meets Passion",
-      description:
-        "Behind 1st Forex is a dynamic and diverse team of forex veterans, successful young traders, and AI specialists. This unique blend of experience and innovation allows us to stay ahead of market trends and deliver unmatched value to our community.",
-      points: [
-        "Forex Veterans: Decades of trading expertise provide the foundation for our education and strategies.",
-        "Young Innovators: Fresh perspectives and modern trading techniques keep us at the cutting edge.",
-        "AI Specialists: Advanced algorithms and data-driven insights enhance our trading tools, making them smarter and more effective.",
-        "Top-Tier Support Team: Our experienced and dedicated professionals work tirelessly to ensure a seamless experience for every trader in our community.",
-      ],
-    },
-  ];
-
   return (
     <Box
       sx={{
