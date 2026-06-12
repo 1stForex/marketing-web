b1e18f76163084e2008f5e3c07ac464928e4e8ad
arham4312 <arhamnadeem4312@gmail.com>
2026-06-12 20:55:27 +0500

    Rename performance nav link

--- src/const/allNavLinks.ts
+++ src/const/allNavLinks.ts
@@ -1,6 +1,6 @@
 export const links = [
   { label: "Home", href: "/home" },
-  { label: "Performance", href: "/historical-performance" },
+  { label: "Historical Results", href: "/historical-performance" },
   { label: "Community", href: "/community" },
   { label: "Pricing", href: "/pricing" },
 ];
